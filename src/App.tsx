ff30d7436690311e818aa9d54be529071d2650cc
Daniel <guimaraes15@gmail.com>
2021-06-25 19:58:10 -0300

    feat: added room user for make questions

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { NewRoom } from './pages/NewRoom';
 import { Home } from './pages/Home';
-
+import { AdminRoom } from './pages/AdminRoom';
 import './styles/global.scss';
 
 import { AuthContextProvider } from './contexts/AuthContext';
@@ -15,6 +15,7 @@ function App() {
           <Route path="/" exact component={Home} />
           <Route path="/rooms/new" exact component={NewRoom} />
           <Route path="/rooms/:id" component={Room} />
+          <Route component={AdminRoom} path="/admin/rooms/:id" />
         </Switch>
       </AuthContextProvider>
     </BrowserRouter>
